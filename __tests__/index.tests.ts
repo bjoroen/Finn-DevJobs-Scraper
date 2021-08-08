72b5f2978f852dec97fb3b0bd3ff8ac3d3ed2877
bjoroen <31513139+bjoroen@users.noreply.github.com>
2021-08-09 01:54:35 +0200

    Should get statuscode 200 from website get req = red

---
+++ __tests__/index.tests.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import * as http from "http";
+import { getRequest } from "../src";
+
+test("Should get status code 200 from website", () => {
+  const url: string = "https://www.finn.no/";
+
+  expect(getRequest(url).req.statusCode).toEqual(400);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Project should be setup correct");
+import https from "https";
+import * as url from "url";
+import * as http from "http";
+
+export const getRequest = (hostname: string) => {
+  https.get(hostname, (res) => {
+    console.log("status Code: ", res.statusCode);
+
+    return res.statusCode;
+  });
+};
+
+const code = getRequest("https://www.finn.no/");
+
+console.log("Yes: ", code);
