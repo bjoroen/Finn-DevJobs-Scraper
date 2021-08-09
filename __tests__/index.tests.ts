cc6ea32e7055d5f08bb48c4f091f72fb9b2534b5
bjoroen <31513139+bjoroen@users.noreply.github.com>
2021-08-09 13:34:04 +0200

    Should get status code 200 from website get req = Green

--- __tests__/index.tests.ts
+++ __tests__/index.tests.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import * as http from "http";
 import { getRequest } from "../src";
+import * as request from "supertest";
 
-test("Should get status code 200 from website", () => {
+test("Should get status code 200 from website", async () => {
   const url: string = "https://www.finn.no/";
 
-  expect(getRequest(url)).toEqual(400);
+  await getRequest(url).then((data) => {
+    expect(data.status).toBe(200);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import https from "https";
+const axios = require("axios").default;
 
-export async function getRequest(hostname: string): Promise<any> {
-  const response = await fetch(hostname);
-  const code = await response.json();
-  const status = code.statusCode;
-  console.log(status);
-  return status;
-}
+export const getRequest = async (hostname: string) => {
+  return axios
+    .get(hostname)
+    .then((response) => {
+      return response;
+    })
+    .catch(console.error);
+};
 
-const test = getRequest("www.finn.no");
-
-console.log(test);
+getRequest("https://www.finn.no/").then((data) => {
+  console.log(data.status);
+});
