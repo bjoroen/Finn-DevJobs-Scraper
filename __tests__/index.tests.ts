b6abdd649c10372ca94a533cf216bc443ce94085
bjoroen <31513139+bjoroen@users.noreply.github.com>
2021-08-14 01:26:09 +0200

    Now getting links and ids from Finn. next get from all pages.

--- __tests__/index.tests.ts
+++ __tests__/index.tests.ts
@@ -1,11 +1,15 @@
-import { getRequest } from "../src";
+import { getLinks, getRequest } from "../src";
+
+const url: string = "https://www.finn.no/";
 
 test("Should get status code 200 from website", async () => {
-  const url: string = "https://www.finn.no/";
-
   await getRequest(url).then((res) => {
     expect(res.status).toBe(200);
   });
 });
 
-test("Should return the Finn ID", () => {});
+test("Should return array with links", async () => {
+  getLinks(url).then((res) => {
+    expect(res[0].link).toMatch(/https:/);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,53 @@ import * as cheerio from "cheerio";
 
 const axios = require("axios").default;
 import Cheerio = cheerio.Cheerio;
+import { stringify } from "ts-jest/dist/utils/json";
+
+export interface links {
+  kode: number;
+  link: string;
+}
+
+let checkIfNextPageTrue: boolean = false;
 
 export async function getRequest(url): Promise<any> {
   return await axios.get(url);
 }
 
-export function getHTML() {
-  const url =
-    "https://www.finn.no/job/fulltime/search.html?abTestKey=control&occupation=0.23&sort=RELEVANCE";
-
-  getRequest(url).then((response) => {
+export function getLinks(url) {
+  return getRequest(url).then((response) => {
+    let listOfLinks: links[] = [];
     const html = response.data;
     const $ = cheerio.load(html);
-    const mainContainer = $("#page-results article").children("div").last();
-    console.log(mainContainer);
+
+    $("#page-results > div.ads.ads--liquid.ads--liquid--cols1to2")
+      .find("article")
+      .find("h2 > a")
+      .each((index, element) => {
+        let temp = {
+          kode: parseInt($(element).attr("id")),
+          link: $(element).attr("href"),
+        };
+
+        listOfLinks.push(temp);
+      });
+    return listOfLinks;
   });
 }
 
-getHTML();
+let pageNumberString: string = "";
+let numberOfPage: string = "";
+let num = 1;
+const url = `https://www.finn.no/job/fulltime/search.html?abTestKey=rerank&occupation=0.23${pageNumberString}${numberOfPage}&sort=PUBLISHED_DESC`;
+
+while (checkIfNextPageTrue === false) {
+  getLinks(url).then((res: links[]) => {
+    console.log("before if");
+    console.log(res.length);
+    pageNumberString = "&page=";
+    num++;
+    numberOfPage = stringify(num);
+  });
+
+  checkIfNextPageTrue = true;
+}
