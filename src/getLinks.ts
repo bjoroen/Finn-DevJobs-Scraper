836cd445c3abdcc09a8cd401afc6c16b6ea5faf1
bjoroen <31513139+bjoroen@users.noreply.github.com>
2021-08-28 14:44:48 +0200

    Started clean up

--- src/getLinks.ts
+++ src/getLinks.ts
@@ -61,5 +61,3 @@ function getAllLinks() {
     }
   }
 }
-
-getAllLinks();
--- src/readingPages.ts
+++ src/readingPages.ts
@@ -6,8 +6,10 @@ import * as cheerio from "cheerio";
 import { getRequest } from "./getLinks";
 import { links } from "./getLinks";
 
+//Array of Links from JSON file
 const linksArray: links[] = JSONdata as links[];
 
+//Languages to check
 const languageArray: string[] = [
   "C",
   "C++",
@@ -18,13 +20,13 @@ const languageArray: string[] = [
   "Javascript",
   "Typescript",
   "Php",
-  "Assembly Language",
 ];
 
 //Check if language is in text
 function languageInText(language: string[], links: string) {
   let text: string = "";
 
+  //Scrapes job AD to see what Languages are in the text
   return getRequest(links).then((response) => {
     let reg: RegExp;
     const html = response.data;
@@ -49,10 +51,11 @@ function languageInText(language: string[], links: string) {
   });
 }
 
+//Counts what languages are being found
 let countMap = new Map();
 
 for (let ele in linksArray) {
-  languageInText(languageArray, linksArray[ele].link).then(async (res) => {
+  languageInText(languageArray, linksArray[ele].link).then((res) => {
     for (let resKey in res) {
       if (countMap.has(res[resKey])) {
         let currentValue: number = countMap.get(res[resKey]);
